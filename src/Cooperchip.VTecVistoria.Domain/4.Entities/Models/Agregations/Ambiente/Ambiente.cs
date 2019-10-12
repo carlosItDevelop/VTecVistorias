2b10b022c22e779bba45b58893011b6a491fc374
Carlos A Santos <carlos.itdevelop@gmail.com>
2019-10-12 17:44:10 -0300

    Modelagem de Ambiente como AgregateRoot

--- src/Cooperchip.VTecVistoria.Domain/4.Entities/Models/Agregations/Ambiente/Ambiente.cs
+++ src/Cooperchip.VTecVistoria.Domain/4.Entities/Models/Agregations/Ambiente/Ambiente.cs
@@ -1,11 +1,26 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
+﻿
+using Cooperchip.VTecVistoria.Domain._4.Entities.Base;
 
 namespace Cooperchip.VTecVistoria.Domain._4.Entities.Models.Agregations.Ambiente
 {
-    public class Ambiente
+    public class Ambiente : EntityBase
     {
+        public Ambiente()
+        {
+        }
+
+        public string Nome { get; set; }
+        public string Sobrenome { get; set; }
+
+        public GaleriaAmbiente GaleriaAmbiente { get; set; }
+        public ItensAmbiente ItensAmbiente { get; set; }
+        public MobiliaAmbiente MobiliaAmbiente { get; set; }
+        public Dano Dano { get; set; }
+
+        public string NomeCompleto()
+        {
+            return this.Nome + " " + this.Sobrenome;
+        }
     }
 
 }
