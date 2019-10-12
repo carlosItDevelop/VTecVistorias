3d880665b8d84523bc5b6e8d25c7e195cb4322ae
Carlos A Santos <carlos.itdevelop@gmail.com>
2019-10-12 17:21:50 -0300

    Add EntityBase Class and Agragated Folder in Domain

---
+++ src/Cooperchip.VTecVistoria.Domain/4.Entities/1.Base/EntityBase.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Cooperchip.VTecVistoria.Domain._4.Entities._1.Base
+{
+    public class EntityBase
+    {
+        public EntityBase()
+        {
+            this.Id = Guid.NewGuid();
+        }
+        public Guid Id { get; set; }
+    }
+}
