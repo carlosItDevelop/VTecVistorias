c1ac897e39fdffb5065bdd12870e1e676d0c4490
Carlos A Santos <carlos.itdevelop@gmail.com>
2019-10-20 02:54:31 -0300

    Imovel com primeiras properties e ctorp + set private

--- src/Cooperchip.VTecVistoria.Domain/4.Entities/Models/Agregations/Imobiliarias/Imovel.cs
+++ src/Cooperchip.VTecVistoria.Domain/4.Entities/Models/Agregations/Imobiliarias/Imovel.cs
@@ -7,5 +7,30 @@ namespace Cooperchip.VTecVistoria.Domain._4.Entities.Models.Agregations.Imobilia
 {
     public class Imovel : EntityBase
     {
+        protected Imovel(string nome, Guid imobiliariaId, Guid locadorId, Guid locatarioId, bool isLocado)
+        {
+            Nome = nome;
+            ImobiliariaId = imobiliariaId;
+            LocadorId = locadorId;
+            LocatarioId = locatarioId;
+            IsLocado = isLocado;
+        }
+
+        public string Nome { get; private set; }
+
+        public Guid ImobiliariaId { get; private set; }
+        public Imobiliaria Imobiliaria { get; private set; }
+
+        public Guid LocadorId { get; private set; }
+        public Locador Locador { get; private set; }
+
+        public Guid LocatarioId { get; private set; }
+        public Locatario Locatario { get; private set; }
+
+        public bool IsLocado { get; private set; }
+
+        // AdRok
+        public void Locar() => IsLocado = true;
+        public void Deslocar() => IsLocado = false;
     }
 }
