9180a65c21bf3c49fcde512c9049909c97d9fc2c
Carlos A Santos <carlos.itdeveloper@gmail.com>
2019-10-21 04:25:59 -0300

    Created IDomainGenericRepository in DomainCore

---
+++ src/Cooperchip.VTecVistoria.DomainCore/Base/IDomainGenericRepository.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+
+namespace Cooperchip.VTecVistoria.DomainCore.Base
+{
+
+    public interface IDomainGenericRepository<TEntity, TKey> : IDisposable
+        where TEntity : class
+    {
+        //Task<List<TEntity>> SelecionarTodos(Expression<Func<TEntity, bool>> quando = null);
+        IEnumerable<TEntity> SelecionarTodos(Expression<Func<TEntity, bool>> quando = null);
+        TEntity SelecionarPorId(TKey id);
+
+        void Inserir(TEntity obj);
+        void Atualizar(TEntity obj);
+        void Excluir(TEntity obj);
+        void ExcluirPorId(TKey id);
+    }
+
+}
